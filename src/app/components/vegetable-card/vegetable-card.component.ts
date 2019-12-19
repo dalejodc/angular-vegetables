cd618b93b2c0dbdbbbfd82c928ee77fd23e2eed7
Daniel Díaz <dalejodc@gmail.com>
2019-12-18 19:51:14 -0600

    feat: input properties

--- src/app/components/vegetable-card/vegetable-card.component.ts
+++ src/app/components/vegetable-card/vegetable-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-vegetable-card',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vegetable-card.component.css']
 })
 export class VegetableCardComponent implements OnInit {
+
+  @Input() name;
+  @Input() abstract;
+  @Input() image;
 
   constructor() { }
 
